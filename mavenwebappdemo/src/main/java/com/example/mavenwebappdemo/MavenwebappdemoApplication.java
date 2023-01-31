2af88ea02073933528e5a96497ff62d39de43635
jellasaikiran <saikiran.jella@zemosolabs.com>
2023-01-31 17:15:44 +0530

    Sample maven apps

---
+++ mavenwebappdemo/src/main/java/com/example/mavenwebappdemo/MavenwebappdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenwebappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenwebappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenwebappdemoApplication.class, args);
+	}
+
+}
---
+++ mavenwebappdemo/src/test/java/com/example/mavenwebappdemo/MavenwebappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenwebappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenwebappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
