249487903ad5e5924586f6608b7c5d0290ab066d
jellasaikiran <saikiran.jella@zemosolabs.com>
2023-02-01 19:43:26 +0530

    Hibernate - One to one bidirectional mapping

---
+++ hibernate-one-to-one-uni/src/main/java/com/ldp/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,41 @@
+package com.ldp.hibernate.demo;
+
+import com.ldp.hibernate.demo.entity.Instructor;
+import com.ldp.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorDetailDemo {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try{
+
+
+            session.beginTransaction();
+
+            int theId = 2;
+            InstructorDetail tempInstructorDetail =
+                    session.get(InstructorDetail.class, theId);
+
+            System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+
+            System.out.println("the associated instructor: " +
+                    tempInstructorDetail.getInstructor());
+
+            session.getTransaction().commit();
+            System.out.println("Successful");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            session.close();
+            sessionFactory.close();
+        }
+
+    }
+}
