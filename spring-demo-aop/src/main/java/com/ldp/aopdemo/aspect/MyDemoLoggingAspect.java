aa6ec5b71f0186500f95d421882233a6c618b2bb
jellasaikiran <saikiran.jella@zemosolabs.com>
2023-02-07 12:47:58 +0530

    AOP - After Advice Type

--- spring-demo-aop/src/main/java/com/ldp/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/main/java/com/ldp/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,17 +2,66 @@ package com.ldp.aopdemo.aspect;
 
 import com.ldp.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Aspect
 @Component
 @Order(1)
 public class MyDemoLoggingAspect {
+    @AfterReturning(
+            pointcut="execution(* com.ldp.aopdemo.dao.AccountDAO.findAccounts(..))",
+            returning="result")
+    public void afterReturningFindAccountsAdvice(
+            JoinPoint theJoinPoint, List<Account> result) {
+
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @AfterReturning on method: " + method);
+        convertAccountNamesToUpperCase(result);
+
+        System.out.println("\n=====>>> result is: " + result);
+
+    }
+
+    @AfterThrowing(
+            pointcut="execution(* com.ldp.aopdemo.dao.AccountDAO.findAccounts(..))",
+            throwing="exe")
+    public void afterThrowingFindAccountsAdvice(
+            JoinPoint theJoinPoint, Throwable exe) {
+
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @AfterThrowing on method: " + method);
+
+        System.out.println("\n=====>>> Exception is: " + exe);
+
+    }
+
+    @After("execution(* com.ldp.aopdemo.dao.AccountDAO.findAccounts(..))")
+    public void afterFinallyFindAccountsAdvice(
+            JoinPoint theJoinPoint) {
+
+        String method = theJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @After finally on method: " + method);
+
+    }
+    private void convertAccountNamesToUpperCase(List<Account> result) {
+
+        // loop through accounts
+
+        for (Account tempAccount : result) {
+
+            // get uppercase version of name
+            String theUpperName = tempAccount.getName().toUpperCase();
+
+            // update the name on the account
+            tempAccount.setName(theUpperName);
+        }
+
+    }
 
     @Before("com.ldp.aopdemo.aspect.AOPExpressions.forDAOPackageNoGetterAndSetter()")
     public void beforeAddAccountAdvice(JoinPoint joinPoint){
--- spring-demo-aop/src/main/java/com/ldp/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop/src/main/java/com/ldp/aopdemo/dao/AccountDAO.java
@@ -3,12 +3,32 @@ package com.ldp.aopdemo.dao;
 import com.ldp.aopdemo.Account;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class AccountDAO {
 
     private String name;
     private String serviceCode;
 
+    public List<Account> findAccounts(boolean b) {
+
+        if(b){
+            throw new RuntimeException("No soup for you!!");
+        }
+        List<Account> myAccounts = new ArrayList<>();
+
+        Account temp1 = new Account("John", "Silver");
+        Account temp2 = new Account("Madhu", "Platinum");
+        Account temp3 = new Account("Luca", "Gold");
+
+        myAccounts.add(temp1);
+        myAccounts.add(temp2);
+        myAccounts.add(temp3);
+
+        return myAccounts;
+    }
     public void addAccount(Account account, boolean flag){
         System.out.println(getClass()+" Doing my add account operation");
     }
