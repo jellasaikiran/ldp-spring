1f85d47951f316974bc9c972d3e1404cac851ffa
jellasaikiran <saikiran.jella@zemosolabs.com>
2023-02-10 18:38:06 +0530

    Spring Boot - Using JPA API

--- crud-demo/src/main/java/com/ldp/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ crud-demo/src/main/java/com/ldp/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.ldp.springboot.cruddemo.service;
 import com.ldp.springboot.cruddemo.dao.EmployeeDAO;
 import com.ldp.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +14,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeDAO employeeDAO;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+    public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl")EmployeeDAO employeeDAO) {
         this.employeeDAO = employeeDAO;
     }
 
